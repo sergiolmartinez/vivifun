473463d303a486d38cf9349ffb22f600496c4e77
sergiolmartinez <smart0586@gmail.com>
2024-07-03 17:14:34 -0700

    added new bubble pop game

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
       <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-black border-t md:flex md:items-center md:justify-between md:p-6 ">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          <a href="/" className="hover:underline">
             Vivi Fun
           </a>
           . All Rights Reserved.
